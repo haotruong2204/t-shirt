29e2d09f75fa4e639510bd7195d04fb9de2ae1ae
Hao Truong <haotv@hblab.vn>
2023-03-17 12:00:34 +0700

    Refs #03 Feature Authentication (layout, devise)

---
+++ app/controllers/administrator/base_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Administrator::BaseController < ApplicationController
+  before_action :authenticate_administrator_administrator!
+end
+  
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import { Turbo } from "@hotwired/turbo-rails";
+import "controllers";
+import "jquery";
+import "lodash";
+import "select2";
+import "flatpickr";
+
+Turbo.session.drive = false;
